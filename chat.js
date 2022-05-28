b3132ca30210d84c977517b01a3f4521419bd216
Carlos Gabriel Antero <58411170+gabriel-antero@users.noreply.github.com>
2022-05-28 00:09:11 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,43 @@
+const socket = io('https://chatinternet.netlify.app/');
+
+let user = null;
+
+socket.on('update_messages', (messages) => {
+    updateMessagesOnScreen(messages)
+})
+
+function updateMessagesOnScreen(messages){
+    const div_messages = document.querySelector('#messages');
+
+    let list_messages = '<ul>'
+    messages.forEach(message=>{
+        list_messages += `<li><strong>${message.user}:</strong> ${message.msg}</li>`
+    })
+    list_messages += `</ul>`
+
+    div_messages.innerHTML = list_messages
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#message_form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        if(!user){
+            alert("Defina um usuario");
+            return;
+        }
+
+        const message = document.forms['message_form_name']['msg'].value;
+        document.forms['message_form_name']['msg'].value = '';
+        socket.emit('new_message', {user: user, msg: message})
+        console.log(message)
+    })
+
+    const userForm = document.querySelector('#user_form');
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        user = document.forms['user_form_name']['user'].value;
+        userForm.parentNode.removeChild(userForm);
+    })
+})
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const socketIo = require('socket.io');
+
+app.use(cors());
+app.use('/', express.static(path.join(__dirname)))
+
+const server = app.listen(3000, ()=>{
+    console.log("Running");
+})
+
+const messages = []
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New Connection')
+    socket.emit('update_messages', messages);
+
+    socket.on('new_message', (data) => {
+        messages.push(data);
+
+        io.emit('update_messages', messages);
+    })
+})
